6cb553f2d6b9c11980fb30ea86920a5ffd0a61c6
githubuserohith <114093355+githubuserohith@users.noreply.github.com>
2024-12-09 20:02:28 +0530

    Add files via upload

---
+++ demo_lin_reg_fit.py
@@ -0,0 +1,15 @@
+import pickle
+from sklearn.linear_model import LinearRegression
+import streamlit as st
+
+X = [[1], [2], [3], [4], [5]]
+y = [2, 3, 5, 8, 11]
+
+model = LinearRegression()
+model.fit(X, y)
+
+# Save the trained model using pickle
+with open('model.pkl', 'wb') as file:
+    pickle.dump(model, file)
+
+st.write("Hi")
